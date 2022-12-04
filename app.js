33d930c1b9c55bb3efd592e2c0956537e25d4423
joao victor <thehoodgamer20@gmail.com>
2022-12-04 13:45:20 -0300

    evento finalizado

---
+++ app.js
@@ -0,0 +1,19 @@
+function massa(id){
+
+let btn = document.querySelector("#btn")
+let btn2 = document.querySelector("#btn2")
+
+if(id==="btn"){
+
+    let btn = document.querySelector("#btn")
+    btn.id = "btn2"
+
+}else{
+
+    let btn2 = document.querySelector("#btn2")
+    btn2.id = "btn"
+
+}
+
+
+}
